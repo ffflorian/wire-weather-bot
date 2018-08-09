135c091df162faf1090e8d21f3f0181916845a3d
Florian Keller <github@floriankeller.de>
2018-08-09 20:34:47 +0200

    Add dotenv, simplify index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-require('dotenv').config();
-
-process.on('uncaughtException', error => console.error(`Uncaught exception: ${error.message}`, error));
-process.on('unhandledRejection', error =>
-  console.error(`Uncaught rejection "${error.constructor.name}": ${error.message}`, error)
-);
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 import {Bot} from '@wireapp/bot-api';
 import {WeatherHandler} from './WeatherHandler';
 import {OwmApiClient as WeatherAPI} from 'openweathermap-api-client';
 
-(async () => {
-  const bot = new Bot({
-    email: String(process.env.WIRE_EMAIL),
-    password: String(process.env.WIRE_PASSWORD),
-  });
-  const weatherAPI = new WeatherAPI({
-    apiKey: String(process.env.API_KEY),
-    lang: 'en',
-    units: 'metric',
-  });
 
-  bot.addHandler(new WeatherHandler(weatherAPI));
+['WIRE_EMAIL', 'WIRE_PASSWORD', 'OPEN_WEATHER_API_KEY'].forEach(envVar => {
+  if (!process.env[envVar]) {
+    throw new Error(`Environment variable "${envVar}" is not set. Please define it or create a .env file.`)
+  }
+})
 
-  await bot.start();
-})();
+const bot = new Bot({
+  email: process.env.WIRE_EMAIL!,
+  password: process.env.WIRE_PASSWORD!,
+});
+
+const weatherAPI = new WeatherAPI({
+  apiKey: process.env.OPEN_WEATHER_API_KEY!,
+  lang: 'en',
+  units: 'metric',
+});
+
+bot.addHandler(new WeatherHandler(weatherAPI));
+bot.start();
